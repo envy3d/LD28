7d8ed5e9d931695eebf018ed135f732c010c5ada
Nick Cooper <nicholas.i.cooper@gmail.com>
2013-12-13 23:51:40 -0500

    Project created

---
+++ LD28-desktop/src/com/envy3d/ld28/Main.java
@@ -0,0 +1,16 @@
+package com.envy3d.ld28;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "LD28";
+		cfg.useGL20 = true;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new LD28Game(), cfg);
+	}
+}
